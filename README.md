6c0f4821d18baf0418393a129ed2d515be73257d
Marcin Winowiecki <mwinowiecki@p110-13.labpk.inf.ug.edu.pl>
2012-11-06 08:53:04 +0100

    Dodane rozwiazanie

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zadania
+
+Lab 2.
+
+zad2. Wyświetl na ekran 4 ostatnie wiersze pliku *program.c*:
+
+```sh
+tainl -n 4 program.c
+```sh
---
+++ c/program1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+  printf("Program ma 10 linijek kodu\n");
+  printf("Teraz cos wypisze...\n");
+  for(int i=0; i<5; i++)
+    printf("Dzien dobry :)\n";
+  printf("Zaraz program sie zakonczy.\n");
+  printf("KONIEC :) \n\n");
+}
