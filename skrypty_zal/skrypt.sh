760774dcaf7d9406db11ad65631da7d2cceab7c0
mwinowiecki <mwinowiecki@sigma.ug.edu.pl>
2013-01-07 22:02:53 +0100

    Update skrypty_zal/skrypt.sh

--- skrypty_zal/skrypt.sh
+++ skrypty_zal/skrypt.sh
@@ -2,21 +2,21 @@
 
 export DISPLAY=:0.0
 
-ping -c 1 komixxy.pl > /dev/null || exit 1
+ping -c 1 komixxy.pl > /dev/null || exit 1    #jesli polaczenie jest -> dalsze dzialanie skryptu
 
 cd /tmp
-test -d komiks && rm -rf komiks
+test -d komiks && rm -rf komiks       #sprawdzenie czy dany folder istnieje i ewentualne usuniecie go
 mkdir komiks
 cd komiks
 
-wget -q http://komixxy.pl/losuj -O komix.html
-egrep bigurl komix.html > adres.txt
-cut -b 15-200 adres.txt > adres1.txt
+wget -q http://komixxy.pl/losuj -O komix.html     #pobranie calej strony
+egrep bigurl komix.html > adres.txt               #wyciagniecie z kodu strony linijki z wyrazeniem 'bigurl'
+cut -b 15-200 adres.txt > adres1.txt              #obciecie poczatku linijki kodu (niestety na "sztywno")
 
 cd ..
-cd komiks
+cd komiks                 #bez tego pozniejsze polecenie 'tr' nie widzialo plikow
 
-cat adres1.txt | tr "\"" " " > adres.txt
-wget -q -i adres.txt -O komix.jpg
+cat adres1.txt | tr "\"" " " > adres.txt      #zamiana ostatniego cudzyslowu na spacje
+wget -q -i adres.txt -O komix.jpg       #pobranie konkretnego obrazka dzieki otrzymanemu wyzej linkowi :)
 
-gwenview -f komix.jpg
+gwenview -f komix.jpg       #otworzenie grafiki przegladarka obrazow (tutaj Gwenview) - nalezy dostosowac do wlasnej dystrybucji
